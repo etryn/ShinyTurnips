3ab474f176a684eff502346a2494366ca8e1bd29
etryn <dave.veronica@gmail.com>
2020-05-16 18:19:02 -0700

    Added all form fields, designated mandatory fields

--- app.R
+++ app.R
@@ -8,41 +8,47 @@
 #
 
 library(shiny)
+library(shinyjs)
+library(base)
+
+# List of Ceylon Participants
+ceylonMembers <- c("Ashley", "Veronica", "Janessa", "Judy", "Andre", "WS", "Dani", "KGB", "Rachel", "Julie")
+
+# Designate Mandatory Fields
+fieldsMandatory <- c("name", "date", "ampm", "price", "firstweek")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui = fluidPage(
+    
+    shinyjs::useShinyjs(),
+    
+    #Define Form
+    titlePanel("Ceylon Stalk Market"),
+    div(
+        id = "form",
+        selectInput("name", "Name", ceylonMembers),
+        dateInput("date", "Date",
+                  value = NULL,
+                  min = "2020-03-19",
+                  max = NULL,
+                  format = "D mm/dd/yy"),
+        radioButtons("ampm", "AM or PM Price",
+                     choiceNames = c("AM", "PM"),
+                     choiceValues = c("am", "pm")),
+        numericInput("price", "Current Price",
+                     value = NULL,
+                     min = 20,
+                     max = 800),
+        checkboxInput("firstweek", "This is my first week buying turnips in my own town.", value = FALSE),
+        actionButton("submit", "Submit", class = "btn-primary")
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+    
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
 }
 
 # Run the application 
